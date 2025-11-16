86a2169ba08ee830f6ded98518fe666fc1f4394c
Aniket Choudhary <aniket834choudhary@gmail.com>
2025-11-16 22:53:01 +0530

    made some optimizations in the code and logic for finding index

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,15 +5,18 @@ const Home = () => {
     const [city ,setCity] = useState("");
     const [data ,setData] = useState([]);
     const [location , setLocation] = useState({});
-    const [weather , setWeather] = useState({currTime : "N/A" ,currTemp : "N/A"});
+    const [weather , setWeather] = useState({});
+    // const [weather , setWeather] = useState({currTime : "N/A" ,currTemp : "N/A"});
     
     async function getCities(){
+        if(!city) return alert("Enter a valid city")
         try {
             const res = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=10&language=en&format=json`);
             if(!res.ok){
                 throw new Error(`http error : ${res.status}`);
             }
             const data = await res.json();
+            if(!data.results) return alert("Enter a valid city")
             setData(data.results)
             
         } catch (error) {
@@ -35,22 +38,22 @@ const Home = () => {
                     }
                     const data = await res.json();
                     console.log(data.hourly);
-                    // data.hourly.time.forEach((time)=>{
+
+                    // earlier approach
+                    // let index=0;
+                    // for(const time of data.hourly.time){
                     //     const convertedTime = new Date(time);
                     //     if (convertedTime.getTime() >= Date.now()){
                     //         console.log(convertedTime);
-                            
+                    //         break;
                     //     }
-                    // })
-                    let index=0;
-                    for(const time of data.hourly.time){
-                        const convertedTime = new Date(time);
-                        if (convertedTime.getTime() >= Date.now()){
-                            console.log(convertedTime);
-                            break;
-                        }
-                        index++;
-                    }
+                    //     index++;
+                    // }
+
+                    let index= data.hourly.time.findIndex(t => new Date(t) > new Date())
+                    if(index === -1 ) index = data.hourly.time.length - 1;
+                    if(index > 0) index-=1;
+
                     const currDate =new Date();
                     setWeather({currDate: currDate.toISOString().split('T')[0] ,currTime : currDate.toLocaleTimeString() ,currTemp : data.hourly.temperature_2m[index] })
                     console.log("current temperature :",data.hourly.temperature_2m[index])
@@ -64,11 +67,6 @@ const Home = () => {
 
     },[location]);
 
-    // const getWeather = (e)=>{
-    //     console.log(e.target)
-    //     console.log(e.target.dataset.latitude)
-    //     console.log(e.target.dataset.longitude)
-    // }
 
   return (
     <>
@@ -79,8 +77,8 @@ const Home = () => {
                 <div >
                     {data.map((el,idx)=>
                     <div key={idx} data-latitude={el.latitude} data-name={el.name} data-longitude={el.longitude} 
-                    className='odd:bg-gray-400 cursor-pointer hover:bg-blue-400 duration-200'
-                    onClick={(e)=>setLocation({name:el.name,latitude:e.target.dataset.latitude,longitude:e.target.dataset.longitude})}
+                    className='odd:bg-gray-300 cursor-pointer hover:bg-blue-400 duration-200'
+                    onClick={(e)=>setLocation({name:el.name,latitude:Number(e.target.dataset.latitude),longitude:Number(e.target.dataset.longitude)})}
                     >
                         {idx+1} {")"} {el.name} ,{el.admin1} , {el.country} , <br /> <b>TimeZone:</b> {el.timezone}
                     </div>)}
@@ -91,13 +89,13 @@ const Home = () => {
                 className='w-1/2 m-auto mt-10'
                 alt="cloudy" />
                 <h2 className='text-xl font-medium mt-10 mx-10'>
-                    Current Time : {weather.currTime}
+                    Current Time : {weather.currTime || "N/A"}
                 </h2>
                 <h2 className='text-xl font-medium mx-10'>
-                    Current Date : {weather.currDate}
+                    Current Date : {weather.currDate || "N/A"}
                 </h2>
                 <h2 className='text-xl font-medium mx-10' >
-                    Current Temperature : {weather.currTemp}
+                    Current Temperature : {weather.currTemp}°C
                 </h2>
             </div>
         </div>
